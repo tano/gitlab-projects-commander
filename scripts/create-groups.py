e616c10f66ba5928a89e12945d49e5f18d1fd583
a.tanoshkin <a.tanoshkin@cian.ru>
2020-05-22 20:52:51 +0300

    Finish create-groups.py script.

--- scripts/create-groups.py
+++ scripts/create-groups.py
@@ -1,30 +1,7 @@
 import os
-import requests
+import urllib.parse
 
-gitlab_hierarchy = {
-    'type': 'group',
-    'name': 'big-group-a',
-    'children': [
-        {
-            'type': 'group',
-            'name': 'big-group-a-nested-1',
-            'children': [
-                {
-                    'type': 'project',
-                    'name': 'big-group-a-nested-1-project-1'
-                },
-                {
-                    'type': 'project',
-                    'name': 'big-group-a-nested-1-project-2'
-                }
-            ]
-        },
-        {
-            'type': 'project',
-            'name': 'big-group-a-project-1'
-        }
-    ]
-}
+import requests
 
 private_token = os.environ.get('GITLAB_PRIVATE_TOKEN')
 
@@ -33,41 +10,53 @@ def main():
     if private_token is None:
         print('please set GITLAB_PRIVATE_TOKEN environment variable')
         exit(1)
-    print('start creating gitlab hierarchy ' + str(gitlab_hierarchy))
+    print('start creating gitlab hierarchy')
     big_a_group_a_id = create_group('big-group-a', 'big-group-a', None)
-    create_project('big-group-a-nested-1-project-1', big_a_group_a_id)
-    big_a_group_a_nested_1_id = create_group('big-group-a-nested-1', 'big-group-a-nested-1',
+    create_project('big-group-a-nested-1-project-1', 'big-group-a/big-group-a-nested-1-project-1', big_a_group_a_id)
+    big_a_group_a_nested_1_id = create_group('big-group-a-nested-1', 'big-group-a/big-group-a-nested-1',
                                              big_a_group_a_id)
-    create_project('big-group-a-nested-1-project-1', big_a_group_a_nested_1_id)
-    create_project('big-group-a-nested-1-project-2', big_a_group_a_nested_1_id)
+    create_project('big-group-a-nested-1-project-1', 'big-group-a/big-group-a-nested-1/big-group-a-nested-1-project-1',
+                   big_a_group_a_nested_1_id)
+    create_project('big-group-a-nested-1-project-2', 'big-group-a/big-group-a-nested-1/big-group-a-nested-1-project-2',
+                   big_a_group_a_nested_1_id)
     print('end creating gitlab hierarchy')
 
 
 def create_group(name, path, parent_id):
-    print('creating group ' + name)
-    creation_request = {'name': name, 'path': path}
-    if parent_id is not None:
-        creation_request['parent_id'] = parent_id
-    response = requests.post('http://localhost/api/v4/groups', data=creation_request,
-                             headers={'Private-Token': private_token})
-    if response.status_code == 201:
-        group_id = response.json()['id']
-        print('successfully created group ' + name + ' with id ' + str(group_id))
-        return group_id
-    elif response.status_code == 400:
-        response = requests.get('http://localhost/api/v4/groups/' + name,
-                                headers={'Private-Token': private_token})
-        group_id = response.json()['id']
+    url_encoded_path = urllib.parse.quote_plus(path)
+    get_group_response = requests.get('http://localhost/api/v4/groups/' + url_encoded_path,
+                                      headers={'Private-Token': private_token})
+    if get_group_response.status_code == 200:
+        group_id = get_group_response.json()['id']
         print('group ' + name + ' already exist, id is ' + str(group_id))
         return group_id
+    elif get_group_response.status_code == 404:
+        print('creating group ' + name)
+        creation_request = {'name': name, 'path': url_encoded_path}
+        if parent_id is not None:
+            creation_request['parent_id'] = parent_id
+        create_group_response = requests.post('http://localhost/api/v4/groups', data=creation_request,
+                                              headers={'Private-Token': private_token})
+        group_id = create_group_response.json()['id']
+        print('successfully created group ' + name + ' with id ' + str(group_id))
+        return group_id
 
 
-def create_project(name, group_id):
-    print('creating project ' + name)
-    response = requests.post('http://localhost/api/v4/groups', data={'name': name, 'namespace_id': group_id},
-                             headers={'Private-Token': private_token})
-    if response.status_code == 201:
-        print('successfully project ' + name)
+def create_project(name, path, group_id):
+    url_encoded_path = urllib.parse.quote_plus(path)
+    get_project_response = requests.get('http://localhost/api/v4/projects/' + url_encoded_path,
+                                        headers={'Private-Token': private_token})
+    if get_project_response.status_code == 200:
+        project_id = get_project_response.json()['id']
+        print('project ' + name + ' already exist, id is ' + str(project_id))
+        return
+    elif get_project_response.status_code == 404:
+        print('creating project ' + name)
+        response = requests.post('http://localhost/api/v4/projects',
+                                 data={'name': name, 'namespace_id': group_id},
+                                 headers={'Private-Token': private_token})
+        if response.status_code == 201:
+            print('successfully project ' + name)
 
 
 if __name__ == '__main__':
